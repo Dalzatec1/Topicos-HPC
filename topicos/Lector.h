a7c266040bbc64d96c6b801449d46c7cad2f032f
Dalzatec1 <dalzatec1@eafit.edu.co>
2020-10-21 17:23:20 -0500

    codigo listo
    código de la aplicación

---
+++ topicos/Lector.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+class Lector{
+public:
+ bool existeEnVector(vector<string> v, string busqueda);
+ void cpaises();
+ void deathcounter();
+
+private:
+    vector<string> paises;
+    string a[12];
+    
+};

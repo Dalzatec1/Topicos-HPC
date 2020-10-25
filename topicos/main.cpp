1d2dab2d4d183530042f70e2dc8c7ec8b28aa3ed
Dalzatec1 <dalzatec1@eafit.edu.co>
2020-10-25 18:58:31 -0500

    d

--- topicos/main.cpp
+++ topicos/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Lector.h"
+#include <fstream>
 using namespace std;
 
 int main() {
@@ -15,8 +16,11 @@ t0=clock();
  l.cpaises();
  l.deathcounter();
 t1 = clock();
-
+ ofstream file;
+file.open("archivo.txt");
+ 
 double time = (double(t1-t0)/CLOCKS_PER_SEC);
-cout << "Execution Time: " << time << endl;  
-
+cout << "Execution Time: " << time << endl; 
+  file<< "Execution Time: "<<time<<"\n"; 
+ file.close();
 }

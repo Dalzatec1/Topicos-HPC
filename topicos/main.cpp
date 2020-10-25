7f62ec90ff9acb82e65eb71acde645d89a23e376
Dalzatec1 <dalzatec1@eafit.edu.co>
2020-10-25 18:31:10 -0500

    archivo final
    final

--- topicos/main.cpp
+++ topicos/main.cpp
@@ -1,106 +1,22 @@
 #include <iostream>
-#include <pthread.h>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include "Lector.h"
 using namespace std;
 
-
-vector<string> paises;
-string a[12];
-
-
-
-    bool existeEnVector(vector<string> v, string busqueda) {
-        return find(v.begin(), v.end(), busqueda) != v.end();
-    }
-
-    void *cpaises(void *tid) {
-        ifstream infile("covid.csv");
-        string line ="";
-        int i=0;
-        while ( getline ( infile, line )){               
-            
-            std::string delimiter = "\",\"";
-            size_t pos = 0;
-            std::string token;
-            while ((pos = line.find(delimiter)) != std::string::npos) {
-                token = line.substr(0, pos);
-                a[i]=token;
-                  line.erase(0, pos + delimiter.length());
-                  i++;
-            }
-            if(!existeEnVector(paises,a[6])){              
-                paises.push_back(a[6]);   
-            }
-          i=0;
-
-        }
-       
-return tid;
-    }
-
-
-    void deathcounter(){
-        string mppais[paises.size()][2];
-        for(int i=0;i<paises.size();++i){
-
-            mppais[i][0]=paises[i];      
-        }
-        int cast;
-            ifstream infile("covid.csv");
-            string line ="";
-            getline ( infile, line );
-            int i=0;
-            int aux=0;
-            int x=0;
-            string dcast="";
-            while ( getline ( infile, line )){               
-            
-                std::string delimiter = "\",\"";
-                size_t pos = 0;
-                string token;
-                while ((pos = line.find(delimiter)) != std::string::npos) {
-                    token = line.substr(0, pos);
-                    a[i]=token;
-                    line.erase(0, pos + delimiter.length());
-                    i++;
-            }
-            if(paises[x]==a[6]){
-                cast=stoi(a[5]);
-                aux+=cast;
-                dcast=to_string(aux);
-                mppais[x][1]=dcast;
-            }
-            else
-            {
-                aux=0;
-                x++;
-                cast=stoi(a[5]);
-                aux+=cast;
-                dcast=to_string(aux);
-                mppais[x][1]=dcast;
-            }
-
-                i=0;
-            }  
-            for(int j=0;i<paises.size();++j){
-
-            cout<<"el pais "+paises[j]+" tiene "+mppais[j][1]+ " muertos"<<endl;
-            }
-         
-        }
-
 int main() {
-   pthread_t thr;
+      Lector l;
 
 
-   pthread_create(&thr, NULL, cpaises, NULL);
-   
-   deathcounter();
-    
   
-    return 0;
+
+ 
+  unsigned t0, t1;
+
+t0=clock();
+ l.cpaises();
+ l.deathcounter();
+t1 = clock();
+
+double time = (double(t1-t0)/CLOCKS_PER_SEC);
+cout << "Execution Time: " << time << endl;  
+
 }
